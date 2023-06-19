fe56ec439853176be08e70a2a330c12903f2b451
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-06-19 19:41:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // set date input with today's date
 const today = new Date().toISOString().split("T")[0];
 dataEl.setAttribute("min", today);
+
+//take values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+//event listeners
+countdownForm.addEventListener("submit", updateCountdown);
