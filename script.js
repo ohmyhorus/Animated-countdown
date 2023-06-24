d4dd4207575883d0da9c9aeb5d4d2ca9d8c46382
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-06-24 17:25:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const timeElements = document.querySelectorAll("span");
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 // set date input with today's date
 const today = new Date().toISOString().split("T")[0];
 dataEl.setAttribute("min", today);
@@ -19,6 +25,23 @@ function updateDOM() {
   const now = new Date().getTime();
   const distance = countdownValue - now;
   console.log("distance", distance);
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minutes);
+  const seconds = Math.floor((distance % minute) / second);
+  console.log(days, hours, minutes, seconds);
+
+  //populate countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+  //hide input
+  inputContainer.hidden = true;
+  //show countdown
+  countdownEl.hidden = false;
 }
 //take values from form input
 function updateCountdown(e) {
